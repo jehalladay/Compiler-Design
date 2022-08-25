0567f8933a356bef44102219c876f62ce31a3198
James Halladay <jameshalladay@Jamess-MacBook-Pro.local>
2022-08-25 10:47:30 -0600

    day 2: finite state machines

--- finite_state_machines/example2.cpp
+++ finite_state_machines/example2.cpp
@@ -1,10 +1,13 @@
-
-
 /*
   
 {+|-}?{0-9}*.{0-9}+{{e|E}{+|-}?{0-9}+}?
 
 */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -14,10 +17,12 @@ enum State {
     POINT,
     POSTDIGIT,
     EXP,
-    EXPNOPLUS,
+    EXPHASSIGN,
     EXPDIGIT,
+    ACCEPT,
     ERROR
 };
+
 
 enum Symbol {
     SIGN,
@@ -27,31 +32,119 @@ enum Symbol {
     OTHER
 };
 
-// State current = LOCKED;
+const int MAX_STATES = 9;
+const int MAX_SYMBOLS = 5;
 
-State doAction(Symbol sym, State state) {
-    // if (current == LOCKED && s == COIN) {
-    //     current = UNLOCKED;
-    // }
-    // if (current == UNLOCKED && s == PUSH) {
-    //     current = LOCKED;
-    // }
+string States[MAX_STATES] = {
+    "NOPLUS",
+    "PREDIGIT",
+    "POINT",
+    "POSTDIGIT",
+    "EXP",
+    "EXPHASSIGN",
+    "EXPDIGIT",
+    "ACCEPT",
+    "ERROR"
+};
 
-    State val = POINT;
+State stt[MAX_SYMBOLS][MAX_STATES] = {
+    // NOPLUS, PREDIGIT, POINT    , POSTDIGIT , EXP       , EXPHASSIGN, EXPDIGIT, ACCEPT, ERROR
+    {PREDIGIT, ERROR   , ERROR    , ACCEPT    , EXPHASSIGN, ERROR     , ACCEPT  , ACCEPT, ERROR}, // SIGN
+    {PREDIGIT, PREDIGIT, POSTDIGIT, POSTDIGIT , EXPDIGIT  , EXPDIGIT  , EXPDIGIT, ACCEPT, ERROR}, // DIGIT
+    {POINT   , POINT   , ERROR    , ACCEPT    , ERROR     , ERROR     , ACCEPT  , ACCEPT, ERROR}, // DECPOINT
+    {ERROR   , ERROR   , ERROR    , EXP       , ERROR     , ERROR     , ACCEPT  , ACCEPT, ERROR}, // E
+    {ERROR   , ERROR   , ERROR    , ACCEPT    , ERROR     , ERROR     , ACCEPT  , ACCEPT, ERROR}, // OTHER
+};
 
-    return val;
+State transition(State current, Symbol s) {
+    return stt[s][current];
 }
 
+Symbol parse(char c) {
 
+    Symbol s = OTHER;
 
+    if (c == '+' || c == '-') {
+        s = SIGN;
+    } else if (c >= '0' && c <= '9') {
+        s = DIGIT;
+    } else if (c == '.') {
+        s = DECPOINT;
+    } else if (c == 'e' || c == 'E') {
+        s = E;
+    }
+
+    return s;
+}
+
+State endState(State current) {
+    if (current == PREDIGIT || current == POSTDIGIT || current == EXPDIGIT) {
+        current = ACCEPT;
+    } 
+
+    return current;
+}
+
+class TestCase { // Pure Abstract Class
+    public:
+    virtual bool doTest() = 0; // Virtual allows subclasses to override
+};
+
+vector<TestCase*> testCases;
+
+class SimpleTests: public TestCase {
+    string test;
+
+    public:
+        SimpleTests(string t = "0.1") {
+            test = t;
+        }
+        
+        bool doTest() {
+            string test = "1.2e3";
+            State current = NOPLUS;
+            Symbol action;
+            
+            for (auto it = test.begin(); it != test.end() && current != ACCEPT && current != ERROR; ++it) {
+                action = parse(*it);
+                current = transition(current, action);
+            }
+            
+            current = endState(current);
+
+            return current == ACCEPT;
+        }
+};
 
 int main() {
 
+    string test = "q+1.0e+1";
     State start = NOPLUS;
-    Symbol nextSymbol = DIGIT;
+    Symbol action = SIGN;
 
-    State current = doAction(nextSymbol, start);
+    State current = NOPLUS;
+    
+    for (auto it = test.begin(); it != test.end() && current != ACCEPT && current != ERROR; ++it) {
+        action = parse(*it);
+        current = transition(current, action);
+    }
 
+    State second = NOPLUS;
+
+    for(auto c:test) {
+        action = parse(c);
+        second = transition(second, action);
+    }
+
+    cout << States[current] << endl;
+    cout << States[second] << endl;
+
+    if (current == ACCEPT || current == POSTDIGIT || current == EXPDIGIT) {
+        cout << "Matches a Floating Point Number" << endl;
+    } else {
+        cerr << "Does not match a Floating Point Number" << endl;
+    }
 
     return 0;
 }
+
